f9b28e73f5e13f2321ebd4eeb20412108b7c63e1
Michal Hoftich <michal.h21@gmail.com>
2020-04-18 14:53:23 +0200

    fixes

--- parse_prir.lua
+++ parse_prir.lua
@@ -75,14 +75,14 @@ end
 
 
 local parse = function(f, root)
-  local root = root or '<section-02>' 
+  local root = root or 'section-02' 
+  root = "<" .. root .. ">"
 	local position = 0
 	local records = {}
 	local r = nil
 	f:seek('set')
 	for line in f:lines() do
 		position = position + 1
-    print(position, ":" .. line .. ":")
 		if line == root then
 			records = save_record(records, r)
 			r, position = new_record(position)
