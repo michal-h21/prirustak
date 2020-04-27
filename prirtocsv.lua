a65b63f1ce7e1f2821b1e8bf5290480aa443625f
Michal Hoftich <michal.h21@gmail.com>
2020-04-27 23:52:14 +0200

    Added field post-processing

--- prirtocsv.lua
+++ prirtocsv.lua
@@ -11,12 +11,16 @@ local map = {
   rok = "z13-year",
   cena = "z30-price",
   druh = "z30-material",
+  isbn = "z13-isbn-issn",
   mail = "email-address",
   dilci = "z30-sub-library",
   sysno = "z30-doc-number",
+  autor = "z13-author",
+  mesto = "z13-imprint",
   popis = "z30-description",
   status = "z30-item-status",
   ctenar = "z303-name",
+  nazev = "z13u-user-defined-2",
   lokace = "z30-collection",
   pujceno = "is-loan",
   bibinfo = "bib-info",
@@ -25,6 +29,7 @@ local map = {
   vypujcek = "z30-no-loans",
   signatura = "z30-call-no",
   dodavatel = "z30-vendor-code",
+  vydavatel = "z13u-user-defined-5",
   id_ctenare = "z303-id",
   signatura2 = "z30-call-no-2",
   zpracovani = "z30-item-process-status",
@@ -59,9 +64,19 @@ local function print_help()
   end
 end
 
+local function parse_format_string(s)
+  local names = {}
+  for name in s:gmatch "%s*([^%,]+)" do
+    names[#names+1] = name
+  end
+  return names
+end
+
 local function parse_format(s)
   local fields = {}
-  for name in s:gmatch "%s*([^%,]+)" do
+  local names = parse_format_string(s)
+  -- for name in s:gmatch "%s*([^%,]+)" do
+  for _, name in ipairs(names) do
     local field = map[name]
     if not field then
       print("Chybí popisek pro pole: " .. name)
@@ -72,9 +87,54 @@ local function parse_format(s)
   end
   return fields
 end
+
+local edits = {
+  autor = function(s)
+    -- odstranit autoritni udaje
+    local autor = s:gsub("[a-zA-Z]+[0-9]+ ???$", "")
+    -- autor ve formatu Prijmeni, Jmeno
+    if s:match("^[^%s]+%s?[^%s]*, ") then
+      -- odstranit rok narozeni
+      autor = autor:gsub("[0-9].+$", "")
+      -- odstranit carku a mezery na konci
+      autor = autor:gsub(",?%s*$", "")
+    else
+      -- korporatni nazev
+    end
+    -- porad tu muzou byt nadbytecne znaky
+    -- nevyhoda je, ze to muze smazat "von" a podobne. ale porad lepsi nez cistit hromadu balastu
+    return autor:gsub(" [a-z]?[a-z]?[a-z]$","")
+  end,
+  vydavatel = function(s)
+    -- nadbytecna carka
+    return s:gsub(",%s*$","") 
+  end,
+  nazev = function(s)
+    -- vrat vsechno po lomitko
+    -- a smaz vsechny uvozovky
+    return s:match("^([^%/]+)"):gsub("'", ""):gsub('"', "") or ""
+  end,
+  isbn = function(s)
+    -- pro jistotu smaz mezery na zacatku, kdyby se nahodou vyskytovaly
+    local s = s:gsub("^%s*", "")
+    -- vrat vsechno po prvni mezeru
+    return s:match("^([^%s]+)") or ""
+  end
+
+}
+
+-- cleanup polí, které to potřebují
+local function edit(fieldname, value)
+  -- zjistit, jestli existuje čistící funkce pro zpracovávané pole
+  local fn = edits[fieldname] or function(val) return val end
+  return fn(value)
+end
     
--- format obsahuje názvy polí
-local function print_csv(format, data)
+-- format obsahuje názvy polí z xml souboru
+-- name_fields nazvy poli predany z CLI
+local function print_csv(format, data, format_string)
+  -- ziskat nazvy poli
+  local name_fields = parse_format_string(format_string)
   -- vytisknout hlavičku
   local function print_header(format)
     local head = format:gsub(",", "\t")
@@ -86,8 +146,8 @@ local function print_csv(format, data)
   for _, row in ipairs(data) do
     local t = {}
     -- projít pole v záznamu a vytvořit tabulku, kterou budeme tisknout
-    for _, f in ipairs(fields) do
-      t[#t+1] = row[f] or ""
+    for i, f in ipairs(fields) do
+      t[#t+1] = edit(name_fields[i], row[f] or "")
     end
     print(table.concat(t,"\t"))
   end
@@ -110,4 +170,4 @@ format)
 parser.make_saves(pos)
 
 local zaz = parser.parse(f)
-print_csv(source_format, zaz)
+print_csv(source_format, zaz,source_format)
